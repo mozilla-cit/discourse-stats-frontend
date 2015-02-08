a5f7222abdf730afb3b80678a7744b6549b48c0e
Yousef Alam <flamingspaz96@googlemail.com>
2015-02-08 00:52:05 +0000

    BAR CHARTS

--- Brocfile.js
+++ Brocfile.js
@@ -5,7 +5,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var app = new EmberApp();
 //app.import('bower_components/ember-charts/ember-charts.css');
 //app.import('bower_components/ember-charts/ember-charts.js');
-//app.import('bower_components/jquery-ui/jquery-ui.min.js');
+app.import('bower_components/jquery/dist/jquery.min.js');
 app.import('bower_components/d3/d3.min.js');
 // Use `app.import` to add additional libraries to the generated
 // output files.
--- app/adapters/category.js
+++ app/adapters/category.js
@@ -3,6 +3,37 @@ import ajax from 'ic-ajax';
 
 export default Ember.Object.extend({
 	find: function() {
-		return ajax(""); // change this to reference window url?
+		return ajax("").then(function(data){
+			var labelData = [];
+			var usersWatching = [];
+			var usersTracking = [];
+			for (var i = 0; i < data.data.length; i++) {
+				labelData[i] = data.data[i].name;
+				usersWatching[i] = data.data[i].users_watching;
+				usersTracking[i] = data.data[i].users_tracking;
+			}
+			return {
+				full: data,
+				chart: {
+					labels: labelData,
+					datasets: [{
+						label: "Watching",
+            			fillColor: "rgba(220,220,220,0.5)",
+            			strokeColor: "rgba(220,220,220,0.8)",
+            			highlightFill: "rgba(220,220,220,0.75)",
+            			highlightStroke: "rgba(220,220,220,1)",
+						data: usersWatching
+					},
+					{
+						label: "Tracking",
+            			fillColor: "rgba(151,187,205,0.5)",
+            			strokeColor: "rgba(151,187,205,0.8)",
+            			highlightFill: "rgba(151,187,205,0.75)",
+            			highlightStroke: "rgba(151,187,205,1)",
+						data: usersTracking
+					}]
+				}
+			};
+		});
 	}
 });
